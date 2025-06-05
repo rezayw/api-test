1ee8c8e1d03e6d1df76ac9c0c200c9faf8acad9c
rezayw <vpldxyw@gmail.com>
2025-06-05 22:44:12 +0700

    finalize mycollection page

--- test-api/src/app/users/dashboard/page.jsx
+++ test-api/src/app/users/dashboard/page.jsx
@@ -1,30 +1,42 @@
 import Image from "next/image";
 import { authUserSession } from "@/libs/auth-libs";
+import Link from "next/link";
 
 const Page = async () => {
   const user = await authUserSession();
 
   return (
-
-    <div className="text-center text-amber-50">
-      {user ? (
-                <div className="text-shadow-amber-50">
-                <h3>DASHBOARD</h3>
-                <h5>Welcome, {user.name}...</h5>
-                <Image
-                    src={user.image}
-                    alt="User Avatar"
-                    width={100}
-                    height={100}
-                    className="rounded-full mx-auto"
-                />
-                </div>
-            ) : (
-                <div>
-                <h3>Dashboard</h3>
-                <h5>Please Sign In to access your dashboard.</h5>
-                </div>
-      )}
+      <div className="mt-8 text-amber-50 flex flex-col justify-center items-center">
+          <h5 className="text-2xl font-bold">Welcome, {user.name}</h5>
+          {user?.image ?(
+          <Image
+              src={user.image}
+              alt="User Avatar"
+              width={100}
+              height={100}
+              className="rounded-full mx-auto"
+          />
+          ) : null }
+          <div className="flex flex-wrap gap-4 py-8">
+            <Link 
+            href="/users/dashboard/collection"
+            className=" bg-amber-300 
+                        text-amber-50
+                        font-bold
+                        px-4
+                        py-3
+                        text-xl"
+            >My Collection</Link>
+            <Link 
+            href="/users/dashboard/comment"
+            className=" bg-amber-300 
+                        text-amber-50
+                        font-bold
+                        px-4
+                        py-3
+                        text-xl"
+            >My Comment</Link>
+          </div>
     </div>
   );
 };
---
+++ test-api/src/components/Dashboard/Header.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { ArrowSquareDownLeftIcon } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
+
+const Header = ({ title }) => {
+    const router = useRouter()
+    
+    const handleBack = (event) => {
+        event.preventDefault()
+        router.back()
+        console.log("Back button clicked")
+    
+    }
+
+    return (
+        <div className="flex justify-between items-center mb-4">
+            <button className="text-amber-50" onClick={handleBack} >
+                <ArrowSquareDownLeftIcon size={32} />
+            </button>
+            <h3 className="text-2xl font-bold text-amber-50">{title}</h3>
+        </div>
+    )
+}
+
+export default Header
